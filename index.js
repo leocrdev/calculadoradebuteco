046fd2200819daefe5f8e085a7ca4f02b87b7eda
Leonardo Cunha Ribeiro <115121765+leocrdev@users.noreply.github.com>
2023-04-28 15:48:12 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function formatMoney(value) {
+    value = Math.ceil(value * 100) / 100
+    value = value.toFixed(2)
+    return 'R$ ' + value
+}
+
+function formatSplit(value) {
+    if (value == 1) return value + ' Pessoa'
+    return value + ' Pessoas'
+}
+
+
+function update(){
+    let bill = Number(document.getElementById('yourBill').value)
+    let tipPercent = document.getElementById('tipInput').value
+    let split = document.getElementById('splitInput').value
+
+    let tipValue = bill * (tipPercent / 100)
+    let billTotal = bill + tipValue
+    let billEach = billTotal  / split
+
+    document.getElementById('tipPercent').innerHTML = tipPercent + ' %'
+    document.getElementById('tipValue').innerHTML = formatMoney(tipValue)
+    document.getElementById('totalWithTip').innerHTML = formatMoney(billTotal)
+    document.getElementById('splitValue').innerHTML = formatSplit(split)
+    document.getElementById('billEach').innerHTML = formatMoney(billTotal / split)
+}
